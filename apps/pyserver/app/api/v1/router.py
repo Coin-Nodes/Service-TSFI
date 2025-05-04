750512e0decb8ea3dcd3aebd953671278b902b21
Cryptoverso <guilherme.loiolasp@gmail.com>
2025-05-04 12:44:38 -0300

    [append]: alternative route

--- apps/pyserver/app/api/v1/router.py
+++ apps/pyserver/app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 
-from apps.pyserver.app.api.v1.NSTW import NSTW, oracle
+from app.api.v1.NSTW import NSTW, oracle
 
 api_router = APIRouter()
 app = FastAPI()
