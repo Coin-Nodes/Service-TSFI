5085a71574dc1daa9fdfdc6926657ce47fa7ea65
Cryptoverso <guilherme.loiolasp@gmail.com>
2025-05-05 02:36:01 -0300

    [append]: cocoa in crud

--- apps/pyserver/app/main.py
+++ apps/pyserver/app/main.py
@@ -3,14 +3,10 @@
 ##  Date:
 ##################################################
 
-import io
-
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-import objectdetection
 from api.v1.router import api_router
 
 TENSORFLOW_URL = "http://tensorflow:8501/v1/models/rfcn:predict"
@@ -27,41 +23,6 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/")
 def home_screen():
     return RedirectResponse(url='/docs')
-
-
-@app.post("/get_predictions/")
-async def get_predictions(file: UploadFile = File(...)):
-    """
-    Submit a picture to be analyzed by the R-FCN model and retrieve the objects
-    identified on the scene.
-    """
-    image_file = await file.read()
-    r = objectdetection.get_predictions(image_file, TENSORFLOW_URL)
-
-    return r
-
-
-@app.post("/get_predicted_image/")
-async def get_predicted_image(file: UploadFile = File(...), detections_limit: int = 20):
-    """
-    Submit a picture to be analyzed by the R-FCN model and get then download the picture
-    with all the objects identified in the scene highlighted.
-
-    - **detections_limit** [int]: Define the limit of objects to be ploted in the returning image.
-    """
-    try:
-        image_file = await file.read()
-        processed_image = objectdetection.get_predicted_image(image_file, TENSORFLOW_URL, detections_limit)
-        filename = file.filename.split(".")[0]
-        extension = file.filename.split(".")[-1]
-        filename = "".join([filename, " (processed).", extension])
-
-        return StreamingResponse(io.BytesIO(processed_image),
-                                 headers={'Content-Disposition': 'attachment; filename=' + filename},
-                                 media_type="image/jpg")
-
-    except Exception as e:
-        return {"error": str(e)}
 
 
 if __name__ == "__main__":
