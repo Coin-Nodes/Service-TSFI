82a3020731141c099c7469ba30c227397374a53c
Cryptoverso <guilherme.loiolasp@gmail.com>
2025-05-05 02:21:51 -0300

    [chore]: nstw

--- apps/pyserver/app/api/v1/router.py
+++ apps/pyserver/app/api/v1/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, FastAPI
 
-from api.v1 import NSTW, oracle
+from api.v1 import nstw, oracle
 
 api_router = APIRouter()
 app = FastAPI()
 
-api_router.include_router(NSTW.router, tags=["NSTW"])
+api_router.include_router(nstw.router, tags=["nstw"])
 api_router.include_router(oracle.router, tags=["oracle"])
