c117f4c5620c29abca6472e779053996927086d6
Cryptoverso <guilherme.loiolasp@gmail.com>
2025-05-05 07:53:10 -0300

    [append]: describe

--- apps/pyserver/app/api/v1/nstw.py
+++ apps/pyserver/app/api/v1/nstw.py
@@ -18,7 +18,19 @@ def hello():
 @router.post("/nstw/")
 async def detect_nstw(file: UploadFile = File(...)):
     """
-    Detecta se uma imagem contém conteúdo NSFW.
+        NSTW Model: Detects Explicit or Implicit Pornography in an Image
+        It returns a value from 0.99 to 0.01. Being 0.99 being safe (99% safe)
+        and 0.01 being unsafe (1% safe)
+
+        Typically, the values indicate the following possible outcomes:
+
+        => 0.99 would be an image without any kind of nudity captured
+            =<0.90 person without pants or shirt, or showing some part of the body, but not pornographic.
+            =< 0.80 person without pants or shirt, or showing some part of the body.
+            =< 0.20 person without pants and without shirt
+            =< 0.10 explicit pornographic content detected.
+
+        Acceptable values to proceed: 0.95 - 0.99
     """
     try:
         image_bytes = await file.read()
@@ -34,7 +46,16 @@ model = predict.load_model("/app/nsfw_model/nsfw_mobilenet2.224x224.h5")
 @router.post("/nsfw/")
 async def detect_nsfw_labels(file: UploadFile = File(...)):
     """
-    Detecta se uma imagem contém conteúdo NSFW.
+    NSFW Model: Detects Explicit or Implicit Pornography com um índice mais detalhado
+    Modelo mais rápido, mas pode ser menos sensíveis em relação a detecção leve de pornografia
+
+    categorias detectadas:
+        "drawings": 0.0057229697704315186,
+        "hentai": 0.0005879989475943148,
+        "neutral": 0.3628992736339569,
+        "porn": 0.011622844263911247,
+        "sexy": 0.619166910648346
+
     """
     try:
         image_bytes = await file.read()
